feeb7aca9ea79bc06e5174fa4a566828e9a377c8
Serhii Krotkykh <sergey.krotkih@gmail.com>
2022-06-27 16:35:11 +0300

    Add SignInButton

---
+++ Sources/SwiftGoogleSignIn/SighUnButton.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//  Created by Serhii Krotkykh on 6/27/22.
+//
+
+import SwiftUI
+import GoogleSignInSwift
+
+/// SwiftUI content view for the Google Sign In
+struct SignInButton: View {
+    var action: () -> ()
+
+    var body: some View {
+        VStack {
+            Spacer()
+            // https://developers.google.com/identity/sign-in/ios/sign-in#4_add_a_google_sign-in_button
+            GoogleSignInButton(scheme: .dark,
+                               style: .standard,
+                               action: {
+                action()
+            })
+            .padding()
+            .frame(width: 100.0)
+            Spacer()
+        }
+    }
+}
