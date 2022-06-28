e2ad94cff3b24db1399a9912c21b6376c6dc02ce
Serhii Krotkykh <sergey.krotkih@gmail.com>
2022-06-28 11:41:51 +0300

    Share SignInButton

--- Sources/SwiftGoogleSignIn/SighUnButton.swift
+++ Sources/SwiftGoogleSignIn/SighUnButton.swift
@@ -9,6 +9,8 @@ import GoogleSignInSwift
 
 /// SwiftUI content view for the Google Sign In
 public struct SignInButton: View {
+    public init() { }
+    
     public var body: some View {
         // https://developers.google.com/identity/sign-in/ios/sign-in#4_add_a_google_sign-in_button
         GoogleSignInButton(scheme: .dark,
