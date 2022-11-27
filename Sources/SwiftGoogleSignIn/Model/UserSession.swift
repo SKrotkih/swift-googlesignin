d56e343d78fda2227801230e22b922f05fecff8f
SKrotkih <sergey.krotkih@gmail.com>
2022-11-27 18:02:56 +0200

    Add isConnected

--- Sources/SwiftGoogleSignIn/Model/UserSession.swift
+++ Sources/SwiftGoogleSignIn/Model/UserSession.swift
@@ -12,25 +12,25 @@ public struct UserSession {
     // MARK: - Properties
     public let profile: UserProfile?
     public let remoteSession: UserAuthentication?
-    public let connectionError: SwiftError?
+    public let error: SwiftError?
     
     // MARK: - Methods
     public init(profile: UserProfile, remoteSession: UserAuthentication) {
         self.profile = profile
         self.remoteSession = remoteSession
-        self.connectionError = nil
+        self.error = nil
     }
 
     public init(error: SwiftError?) {
         self.profile = nil
         self.remoteSession = nil
-        self.connectionError = error
+        self.error = error
     }
     
-    static var keyName: String {
-        return String(describing: self)
+    public var isConnected: Bool {
+        return profile != nil && remoteSession != nil
     }
-    
+
     static var empty: UserSession {
         return UserSession(error: nil)
     }
